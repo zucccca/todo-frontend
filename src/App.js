e9f43dfae0b46f1181e5c476522ba2ae17f73505
John Zuccarelli <j.zuccarelli22@gmail.com>
2026-01-09 19:26:22 -0500

    basic todo fetching setup initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    const fetchTodos = async () => {
+      const allTodos = await fetch("http://localhost:3001/todos");
+      const data = await allTodos.json();
+      console.log("data", data);
+
+      setTodos(data.todos);
+    };
+    fetchTodos();
+  }, []);
+
+  const renderTodos = () => {
+    return (
+      <ul>
+        {todos.map((todo) => (
+          <li>{todo.title}</li>
+        ))}
+      </ul>
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {todos.length ? renderTodos() : <h1>Fetching todos...</h1>}
     </div>
   );
 }

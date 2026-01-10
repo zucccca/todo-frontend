928c266f8a0fa23d746cd5d593707a9e4f52b54b
John Zuccarelli <j.zuccarelli22@gmail.com>
2026-01-10 12:30:03 -0500

    add form to add new todo

--- src/App.js
+++ src/App.js
@@ -3,16 +3,33 @@ import "./App.css";
 
 function App() {
   const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
+
   useEffect(() => {
     const fetchTodos = async () => {
       const allTodos = await fetch("http://localhost:3001/todos");
       const data = await allTodos.json();
-      console.log("data", data);
 
       setTodos(data.todos);
     };
     fetchTodos();
   }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const url = "http://localhost:3001/todos";
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title: todo }),
+    });
+
+    const createdTodo = await response.json();
+    setTodos([...todos, createdTodo]);
+    setTodo("");
+  };
 
   const renderTodos = () => {
     return (
@@ -27,6 +44,16 @@ function App() {
   return (
     <div className="App">
       {todos.length ? renderTodos() : <h1>Fetching todos...</h1>}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="todo">Add todo</label>
+        <input
+          type="text"
+          name="todo"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
+        <button type="submit">Add</button>
+      </form>
     </div>
   );
 }
